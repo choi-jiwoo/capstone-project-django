febfca808888390b7ee49327260d79ac7d506716
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-07 00:48:02 +0900

    feat: integrate with legacy database

--- jeju/api/models.py
+++ jeju/api/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class SampleStoreList(models.Model):
+    seq = models.CharField(primary_key=True, max_length=20, blank=True, null=False)
+    regdate = models.CharField(db_column='regDate', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    simplenm = models.CharField(db_column='simpleNm', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    zipcd = models.CharField(db_column='zipCd', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    addr = models.CharField(max_length=50, blank=True, null=True)
+    telephone = models.CharField(max_length=15, blank=True, null=True)
+    telno = models.CharField(db_column='telNo', max_length=15, blank=True, null=True)  # Field name made lowercase.
+    biztype = models.CharField(db_column='bizType', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    biztypenm = models.CharField(db_column='bizTypeNm', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    biztypedetail = models.CharField(db_column='bizTypeDetail', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    salescd = models.CharField(db_column='salesCd', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    latitude = models.CharField(max_length=15, blank=True, null=True)
+    longitude = models.CharField(max_length=15, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sample_store_list'
--- jeju/jeju/settings.py
+++ jeju/jeju/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from configparser import ConfigParser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +74,22 @@ WSGI_APPLICATION = 'jeju.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+parser = ConfigParser()
+parser.read('./config.conf')
+database = parser.get('mysql_config', 'database')
+username = parser.get('mysql_config', 'username')
+password = parser.get('mysql_config', 'password')
+host = parser.get('mysql_config', 'hostname')
+port =parser.get('mysql_config', 'port')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': database,
+        'USER': username,
+        'PASSWORD': password,
+        'HOST': host,
+        'PORT': port,
     }
 }
 
