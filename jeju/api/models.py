c5de13c269fb52c36e0fdbe5b97475e3b3e1f49c
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-07 23:04:49 +0900

    feat: add SampleCategory

--- jeju/api/models.py
+++ jeju/api/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+class SampleCategory(models.Model):
+    id = models.IntegerField(primary_key=True)
+    seq = models.ForeignKey('SampleStoreList', models.DO_NOTHING, db_column='seq')
+    category = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'sample_category'
+
+
 class SampleStoreList(models.Model):
     seq = models.CharField(primary_key=True, max_length=20)
     simplenm = models.CharField(db_column='simpleNm', max_length=20, blank=True, null=True)  # Field name made lowercase.
