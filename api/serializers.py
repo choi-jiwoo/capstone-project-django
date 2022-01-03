43f81257d095f0403c4261e1283f1bd9d4f29056
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-03 15:46:32 +0900

    refactor: remove serializers of SampleCategory and SampleStoreLIst

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,1 @@
 from rest_framework.serializers import ModelSerializer
-from api.models import SampleCategory, SampleStoreList
-
-
-class DataSerializer(ModelSerializer):
-    class Meta:
-        model = SampleCategory
-        fields = '__all__'
-
-class StoreSerializer(ModelSerializer):
-    class Meta:
-        model = SampleStoreList
-        fields = '__all__'
-
