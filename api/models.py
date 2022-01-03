0f8881fc73e3869cfeaf9d0512c5291d0f05a9c8
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-03 15:30:42 +0900

    refactor: remove SampleCategory and SampleStoreList models

--- api/models.py
+++ api/models.py
@@ -1,24 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
-class SampleCategory(models.Model):
     id = models.IntegerField(primary_key=True)
-    seq = models.ForeignKey('SampleStoreList', models.DO_NOTHING, db_column='seq')
-    category = models.IntegerField()
 
     class Meta:
         managed = False
-        db_table = 'sample_category'
-
-
-class SampleStoreList(models.Model):
-    seq = models.CharField(primary_key=True, max_length=20)
-    simplenm = models.CharField(db_column='simpleNm', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    category = models.IntegerField(blank=True, null=True)
-    latitude = models.CharField(max_length=15, blank=True, null=True)
-    longitude = models.CharField(max_length=15, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'sample_store_list'
