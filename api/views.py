24106d307e5e2758144d08f5f5789de95bd47853
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-03 16:01:03 +0900

    feat: add view for Stay

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
+from api.models import Stay
+from api.serializers import StaySerializer
+
+
+# stay
+@api_view(['GET'])
+def get_filter_stay(request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    params = request.query_params
+    district = params['district']
+    data = Stay.objects.filter(district=district)
+    result_page = paginator.paginate_queryset(data, request)
+    serializer = StaySerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
