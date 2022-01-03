8bca0c5b40d96ba7879e3f37cfd5a26eab12c276
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-03 16:01:25 +0900

    feat: add StaySerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework.serializers import ModelSerializer
+from api.models import Stay
+
+
+class StaySerializer(ModelSerializer):
+    class Meta:
+        model = Stay
+        fields = '__all__'
