19d1e063df9751bfe56e13a80532c68788b1a5f4
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-03 15:30:58 +0900

    feat: add Stay model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
+class Stay(models.Model):
     id = models.IntegerField(primary_key=True)
+    code = models.CharField(max_length=20, blank=True, null=True)
+    name = models.CharField(max_length=10, blank=True, null=True)
+    address = models.CharField(max_length=50, blank=True, null=True)
+    district = models.CharField(max_length=50, blank=True, null=True)
+    numofrooms = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
+        db_table = 'stay'
