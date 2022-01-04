2e18b5fa3cfce0e06956df1a1e05c7572683af1b
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-04 15:39:41 +0900

    feat: add serializers of models Cafe, Res, CafeTag, ResTag

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework.serializers import ModelSerializer
-from api.models import Stay
+from api.models import Stay, Cafe, Res, CafeTag, ResTag
 
 
 class StaySerializer(ModelSerializer):
     class Meta:
         model = Stay
+        fields = '__all__'
+
+
+class CafeSerializer(ModelSerializer):
+    class Meta:
+        model = Cafe
+        fields = '__all__'
+
+
+class ResSerializer(ModelSerializer):
+    class Meta:
+        model = Res
+        fields = '__all__'
+
+
+class CafeTagSerializer(ModelSerializer):
+    class Meta:
+        model = CafeTag
+        fields = '__all__'
+
+
+class ResTagSerializer(ModelSerializer):
+    class Meta:
+        model = ResTag
         fields = '__all__'
