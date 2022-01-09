c5766a76ba1cf58d9eabd9bbf557a5a5a8b977e0
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-09 22:50:43 +0900

    feat: add get_cafe_kwrds and get_res_kwrds view

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from api import views
 
 urlpatterns = [
     path('cafe', views.get_all_cafe),
+    path('cafe/keyword', views.get_cafe_kwrds),
+    path('restaurant/keyword', views.get_res_kwrds),
 ]
--- api/views.py
+++ api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
-from api.models import Stay, Cafe, Res, CafeTag, ResTag
+from api.models import Stay, Cafe, Res, CafeTag, ResTag, CafeKwrd, ResKwrd
 from api.serializers import StaySerializer, CafeSerializer, ResSerializer
 from api.serializers import CafeTagSerializer, ResTagSerializer
+from api.serializers import CafeKwrdSerializer, ResKwrdSerializer
 
 
 # cafe
@@ -16,6 +17,19 @@ def get_all_cafe(request):
     result_page = paginator.paginate_queryset(data, request)
     serializer = CafeSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
+# keywords
+@api_view(['GET'])
+def get_cafe_kwrds(request):
+    data = CafeKwrd.objects.all().order_by('id')
+    serializer = CafeKwrdSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_res_kwrds(request):
+    data = ResKwrd.objects.all().order_by('id')
+    serializer = ResKwrdSerializer(data, many=True)
+    return Response(serializer.data)
+
 # stay
 @api_view(['GET'])
 def get_filter_stay(request):
